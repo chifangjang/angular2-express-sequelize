6329aa80b8b0588f54cd5de841caddcea4016ce3
chifangjang <chifangjang@gmail.com>
2017-05-28 02:39:46 +0800

    1. add gitignore 2. fixed error 404 for typescript 3. fixed error when db should set default database

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@
 var fs        = require("fs");
 var path      = require("path");
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize(process.env.POSTGRESQL_LOCAL_DB, "", "", {
+var sequelize = new Sequelize(process.env.POSTGRESQL_LOCAL_DB || 'test', "", "", {
     host : 'localhost',
     dialect: 'postgres',
 
